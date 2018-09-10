b4e6c403276ad99f6175f9e73967e938e18b6c84
matthexx <matthew_imadiyi@yahoo.com>
2018-09-10 15:59:46 +0100

    first

---
+++ TVUIComponent/lib/src/main/java/com/amazon/android/tv/tenfoot/ui/accounts/ForgotPassActivity.java
@@ -0,0 +1,15 @@
+package com.amazon.android.tv.tenfoot.ui.accounts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amazon.android.tv.tenfoot.R;
+
+public class ForgotPassActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass);
+    }
+}
---
+++ TVUIComponent/lib/src/main/java/com/amazon/android/tv/tenfoot/ui/accounts/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.amazon.android.tv.tenfoot.ui.accounts;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.amazon.android.tv.tenfoot.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
